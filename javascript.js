3e09839fba6e2c0f722b4c6f838f1212e0812f24
turistakiller <78664165+turistakiller@users.noreply.github.com>
2024-12-18 19:21:19 -0300

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,11 @@
+function confirmAgreement() {
+  // Exibe o loader
+  const loader = document.getElementById("loader");
+  loader.classList.remove("hidden");
+
+  // Simula a análise de dados (3 segundos)
+  setTimeout(() => {
+    loader.classList.add("hidden");
+    alert("Parabéns! Você aceitou o acordo. Entraremos em contato para finalizar.");
+  }, 3000);
+}
