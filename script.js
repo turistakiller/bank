99e5f2f8f38b8d09195ab65193ab5c6464dd1be8
turistakiller <78664165+turistakiller@users.noreply.github.com>
2024-12-18 19:26:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function confirmAgreement() {
-  // Exibe o loader
-  const loader = document.getElementById("loader");
-  loader.classList.remove("hidden");
+  // Exibe o modal
+  const loaderModal = document.getElementById("loaderModal");
+  loaderModal.classList.remove("hidden");
 
   // Simula a análise de dados (3 segundos)
   setTimeout(() => {
-    loader.classList.add("hidden");
+    loaderModal.classList.add("hidden");
     alert("Parabéns! Você aceitou o acordo. Entraremos em contato para finalizar.");
-  }, 30000);
+  }, 3000);
 }
